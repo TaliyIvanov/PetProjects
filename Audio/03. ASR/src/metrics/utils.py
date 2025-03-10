0b57a2de1a0010ec00f5dc3bf90fd46875f332d6
TaliyIvanov <pavlovv1993@gmail.com>
2025-03-10 16:44:29 +0300

    write wer & cer, change some configs

--- Audio/03. ASR/src/metrics/utils.py
+++ Audio/03. ASR/src/metrics/utils.py
@@ -1,13 +1,17 @@
+# library for fast quick calculation of edit distance
+import editdistance
 # Based on seminar materials
 
 # Don't forget to support cases when target_text == ''
 
 
-def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
-
-
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    if len(target_text) == 0:
+        return 1
+    return editdistance(target_text.split(), predicted_text.split())/len(target_text.split())
+
+
+def calc_cer(target_text, predicted_text) -> float:
+    if len(target_text) == 0:
+        return 1
+    return editdistance.eval(target_text, predicted_text)/len(target_text)
--- Audio/03. ASR/src/models/__init__.py
+++ Audio/03. ASR/src/models/__init__.py
@@ -1,4 +1,5 @@
 from src.models.deepspeech2 import DeepSpeech2
+from src.models.baseline_model import BaselineModel
 
 __all__ = [
     "BaselineModel", "DeepSpeech2",
