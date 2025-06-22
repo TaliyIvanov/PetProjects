85e9b4035c8b473d03da5af4451488f2c395daba
TaliyIvanov <pavlovv1993@gmai.com>
2025-06-22 19:57:54 +0300

    detect numbers

--- CV/07.CarNumberPlates/main.py
+++ CV/07.CarNumberPlates/main.py
@@ -4,6 +4,14 @@ from ultralytics import YOLO
 import numpy as np
 import sort as s
 import easyocr
+import os
+
+# create numbers plates
+cascade_path = os.path.join(cv2.data.haarcascades, "haarcascade_russian_plate_number.xml")
+nPlateCascade = cv2.CascadeClassifier(cascade_path)
+numbercolor = (0, 0, 255)
+minarea = 200
+scale = 0.5
 
 # load video
 cap = cv2.VideoCapture('data/test_video.mp4')
@@ -21,21 +29,50 @@ if cap is None or not cap.isOpened():
 
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         print('Exiting...')
-#         breakS
-    
-cv2.destroyAllWindows()
+#         break
+# cv2.destroyAllWindows()
 
 # load model
-
+model = YOLO('models/yolov8n.pt')
 
 # pipeline
 while True:
     success, frame = cap.read()
+    if not success:
+        break
+    # prepair image
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    frame = frame[400:1520,:,:]
+    height, width = frame.shape[:2]
+    frame = cv2.resize(frame, (int(width * scale), int(height * scale)))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# detect car with Yolo8n
+    # detect car with Yolo8n
+
+    # detect the number plate with opencv
+    numberPlates = nPlateCascade.detectMultiScale(gray, 1.1, 10)
+    for (x,y,w,h) in numberPlates:
+        area = w * h
+        if area > minarea:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), numbercolor, 1)
+            cv2.putText(frame, 'NumberPlate', (x, y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, numbercolor, 1)
+            frame_number = frame[y:y+h, x:x+w, :]
+            
+            cv2.imshow('Number', frame_number) # show numberplate
+
+            # prepair numberplate for easyOCR
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print('Exiting...')
+        break
+
+cv2.destroyAllWindows()
 
 
-# detect the number plate with opencv
+
+
 
 # OCR the number text with easyOCR
 
